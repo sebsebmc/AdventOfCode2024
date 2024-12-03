2c0b4b3694af3552a23d1330e0c3150c75aab4a4
Sebastian Chlopecki <sebscoolmail@gmail.com>
2024-12-02 18:04:41 -0600

    Day2 and testing setup

---
+++ AdventOfCode/src/Day01.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode;
+
+public class Day01 : BaseDay
+{
+    private readonly string _input;
+
+    public Day01()
+    {
+        _input = File.ReadAllText(InputFilePath);
+
+    }
+
+    private (List<int>, List<int>) getLists() {
+        var list1 = new List<int>();
+        var list2 =  new List<int>();
+
+        foreach(var line in _input.Split("\n")[..^1]){
+            var words = line.Split("   ");
+            list1.Add(Int32.Parse(words[0]));
+            list2.Add(Int32.Parse(words[1]));
+        }
+        return (list1, list2);
+    }
+
+    public override ValueTask<string> Solve_1()  {
+        var (list1, list2) = getLists();
+
+        list1.Sort();
+        list2.Sort();
+
+        var sum = 0;
+        for (int i = 0; i < list1.Count; i++)
+        {
+            sum += Math.Abs(list1[i] - list2[i]);
+        }
+        return new ValueTask<string>(sum.ToString());
+    }
+
+    public override ValueTask<string> Solve_2() {
+        var (list1, list2) = getLists();
+
+        var freq1 = list1.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+        var freq2 = list2.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+        var sum = 0;
+        foreach(var num in freq1){
+            sum += num.Key * num.Value * freq2.GetValueOrDefault(num.Key, 0); 
+        }
+
+        return new ValueTask<string>(sum.ToString());
+    }
+}
---
+++ AdventOfCode/test/AdventOfCode.Testing/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCode;
+using AoCHelper;
+using NUnit.Framework;
+
+namespace AdventOfCode.Testing;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [TestCase(typeof(Day01), "11", "31")]
+    [TestCase(typeof(Day02), "2", "4")]
+    public async Task Test(Type type, string sol1, string sol2)
+    {
+        // Can't use BaseDay since some of them aren't days, but you probably can
+        if (Activator.CreateInstance(type) is BaseDay instance)
+        {
+            Assert.That(sol1, Is.EqualTo(await instance.Solve_1()));
+            Assert.That(sol2, Is.EqualTo(await instance.Solve_2()));
+        }
+        else
+        {
+            Assert.Fail($"{type} is not a BaseProblem");
+        }
+    }
+}
